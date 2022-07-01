46991cd09d310357eea544058bfed6b5eb7d00c7
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-07-01 16:10:16 -0300

    requisito 1 feito - ajuste de pastas

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { loginAction } from '../redux/actions';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
   state= {
